62d0510fffefec53f6f0b9a0db56af6ad3fd1b35
Xavier Carty <xaviercarty@gmail.com>
2020-07-10 21:56:13 -0400

    Updates

---
+++ precache-manifest.9ccc852e8c6dd6a4d695871124a4c84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822bdb567c296f61340642c22b760428",
+    "url": "/SpakFrontEnd/index.html"
+  },
+  {
+    "revision": "47d1b4ebf9c9736eab55",
+    "url": "/SpakFrontEnd/static/css/main.990380e2.chunk.css"
+  },
+  {
+    "revision": "2ef577f29c117e5730d7",
+    "url": "/SpakFrontEnd/static/js/2.2ec9f008.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SpakFrontEnd/static/js/2.2ec9f008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d1b4ebf9c9736eab55",
+    "url": "/SpakFrontEnd/static/js/main.4c1e08ff.chunk.js"
+  },
+  {
+    "revision": "80279cf5b7f60abe9d68",
+    "url": "/SpakFrontEnd/static/js/runtime-main.d477eca0.js"
+  }
+]);
