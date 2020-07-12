bfdcad8b3603c1f96f7dc3ad74680f7d6cf7128f
Xavier Carty <xaviercarty@gmail.com>
2020-07-12 10:04:33 -0400

    my code

--- spak-project/src/App.js
+++ spak-project/src/App.js
@@ -119,7 +119,7 @@ export class App extends Component {
 
 handleHome = (routerProps) => {
   
-  if(routerProps.location.pathname === "/"){
+  if(routerProps.location.pathname === "/" || routerProps.location.pathname === "/SpakFrontEnd/"){
     return <Home  users={this.state.user} />
   }
 
